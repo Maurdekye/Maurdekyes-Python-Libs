25e8781a8d25d6f6e2fa17a471a66270686f1c96
Maurdekye <ncolaprete@gmail.com>
2017-11-06 16:05:14 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="MPyLibs",
+    version="1.0",
+    packages=find_packages(),
+    author="Maurdekye",
+    description="A collection of python libs i've written"
+)
